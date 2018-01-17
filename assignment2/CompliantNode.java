5bc4e1ce0973eed2b5e295ffc53a743a53c630da
dunkthelunk <prudhvi.porandla@icloud.com>
2018-01-18 00:53:05 +0530

    Consensus Protocol: broadcast tx if not seen before

--- assignment2/CompliantNode.java
+++ assignment2/CompliantNode.java
@@ -1,26 +1,47 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    private double p_graph;
+    private double p_malicious;
+    private double p_txDistribution;
+    private int numRoundsLeft;
 
-    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+    private Set<Transaction> pendingTransactions;
+    private Set<Transaction> newTransactionsReceivedInThisRound;
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRoundsLeft) {
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRoundsLeft = numRoundsLeft;
     }
 
+    @Override
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
     }
 
+    @Override
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.pendingTransactions = pendingTransactions;
+        this.newTransactionsReceivedInThisRound = new HashSet<>(pendingTransactions);
     }
 
+    @Override
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        if(numRoundsLeft == 0) {
+            return pendingTransactions;
+        }
+        numRoundsLeft--;
+        pendingTransactions.addAll(newTransactionsReceivedInThisRound);
+        return newTransactionsReceivedInThisRound;
     }
 
+    @Override
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+       newTransactionsReceivedInThisRound = candidates.stream().map(candidate -> candidate.tx).filter(tx -> !pendingTransactions.contains(tx)).collect(Collectors.toSet());
     }
 }
