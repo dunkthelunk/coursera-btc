b05c175e664399f088b24781bdb109418e6037a0
dunkthelunk <prudhvi.porandla@icloud.com>
2018-01-20 18:19:51 +0530

    broadcast new txs; unfollow if inactive or spams.

--- assignment2/CompliantNode.java
+++ assignment2/CompliantNode.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
@@ -8,6 +13,10 @@ public class CompliantNode implements Node {
   private int numRoundsLeft;
 
   private Set<Transaction> pendingTransactions;
+  private Set<Transaction> txsToTransmit;
+  private Map<Integer, Set<Transaction>> mapOfFolloweesAndTheirMessages;
+  private Map<Integer, Integer> numOfRoundsFolloweeDidntRespond;
+  private boolean[] followees;
 
   public CompliantNode(
       double p_graph, double p_malicious, double p_txDistribution, int numRoundsLeft) {
@@ -15,25 +24,63 @@ public class CompliantNode implements Node {
     this.p_malicious = p_malicious;
     this.p_txDistribution = p_txDistribution;
     this.numRoundsLeft = numRoundsLeft;
+    mapOfFolloweesAndTheirMessages = new HashMap<>();
+    numOfRoundsFolloweeDidntRespond = new HashMap<>();
   }
 
   @Override
   public void setFollowees(boolean[] followees) {
-    // IMPLEMENT THIS
+    this.followees = followees;
+    //    IntStream.range(0, followees.length).forEach( i ->
+    // numOfRoundsFolloweeDidntRespond.put(i,0));
   }
 
   @Override
   public void setPendingTransaction(Set<Transaction> pendingTransactions) {
     this.pendingTransactions = pendingTransactions;
+    txsToTransmit = new HashSet<>(pendingTransactions);
   }
 
   @Override
   public Set<Transaction> sendToFollowers() {
-    return pendingTransactions;
+    if (numRoundsLeft == 0) {
+      return pendingTransactions;
+    }
+    numRoundsLeft--;
+    return txsToTransmit;
   }
 
   @Override
   public void receiveFromFollowees(Set<Candidate> candidates) {
-    candidates.stream().map(candidate -> candidate.tx).forEach(pendingTransactions::add);
+    txsToTransmit = new HashSet<>();
+    Set<Integer> senders = candidates.stream().map(c -> c.sender).collect(Collectors.toSet());
+    IntStream.range(0, followees.length)
+        .forEach(
+            i -> {
+              if (followees[i] && !senders.contains(i)) {
+                numOfRoundsFolloweeDidntRespond.merge(i, 1, Integer::sum);
+                if (numOfRoundsFolloweeDidntRespond.get(i) > 2) {
+                  followees[i] = false;
+                }
+              }
+            });
+    candidates
+        .stream()
+        .forEach(
+            c -> {
+              if (!followees[c.sender]) {
+                return;
+              }
+              mapOfFolloweesAndTheirMessages.putIfAbsent(c.sender, new HashSet<>());
+              if (mapOfFolloweesAndTheirMessages.get(c.sender).contains(c.tx)) {
+                followees[c.sender] = false;
+              } else {
+                mapOfFolloweesAndTheirMessages.get(c.sender).add(c.tx);
+                if (!pendingTransactions.contains(c.tx)) {
+                  txsToTransmit.add(c.tx);
+                }
+              }
+            });
+    pendingTransactions.addAll(txsToTransmit);
   }
 }
