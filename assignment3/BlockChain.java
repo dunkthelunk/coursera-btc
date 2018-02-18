0644afde9b75fb1a906134d8aafa3afc2892e905
dunkthelunk <prudhvi.porandla@icloud.com>
2018-02-18 18:17:12 +0530

    blockchain impl1. score - 86

--- assignment3/BlockChain.java
+++ assignment3/BlockChain.java
@@ -1,51 +1,116 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
 // Block Chain should maintain only limited block nodes to satisfy the functions
-// You should not have all the blocks added to the block chain in memory 
+// You should not have all the blocks added to the block chain in memory
 // as it would cause a memory overflow.
 
 public class BlockChain {
-    public static final int CUT_OFF_AGE = 10;
+  public static final int CUT_OFF_AGE = 10;
+  private TransactionPool transactionPool;
+  private Map<ByteArrayWrapper, UTXOPool> utxoPoolMap;
+  private LinkedBlockingQueue<List<Block>> blocksInMemory;
+  private Map<ByteArrayWrapper, Integer> blockHeightMap;
+  private int minHeightInMem;
+  /**
+   * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
+   * block
+   */
+  public BlockChain(Block genesisBlock) {
+    minHeightInMem = 0;
+    transactionPool = new TransactionPool();
+    blocksInMemory = new LinkedBlockingQueue<>(CUT_OFF_AGE);
+    List<Block> blocksAtHeight0 = Collections.singletonList(genesisBlock);
+    blocksInMemory.offer(blocksAtHeight0);
+    utxoPoolMap = new HashMap<>();
+    UTXOPool genesisBlockUtxoPool = new UTXOPool();
+    genesisBlockUtxoPool.addUTXO(
+        new UTXO(genesisBlock.getCoinbase().getHash(), 0), genesisBlock.getCoinbase().getOutput(0));
+    utxoPoolMap.put(new ByteArrayWrapper(genesisBlock.getHash()), genesisBlockUtxoPool);
+    blockHeightMap = new HashMap<>();
+    blockHeightMap.put(new ByteArrayWrapper(genesisBlock.getHash()), minHeightInMem);
+  }
 
-    /**
-     * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
-     * block
-     */
-    public BlockChain(Block genesisBlock) {
-        // IMPLEMENT THIS
-    }
+  /** Get the maximum height block */
+  public Block getMaxHeightBlock() {
+    int queueSize = blocksInMemory.size();
+    return (Block) blocksInMemory.toArray(new List[queueSize])[queueSize - 1].get(0);
+  }
 
-    /** Get the maximum height block */
-    public Block getMaxHeightBlock() {
-        // IMPLEMENT THIS
-    }
+  /** Get the UTXOPool for mining a new block on top of max height block */
+  public UTXOPool getMaxHeightUTXOPool() {
+    return utxoPoolMap.get(new ByteArrayWrapper(getMaxHeightBlock().getHash()));
+  }
 
-    /** Get the UTXOPool for mining a new block on top of max height block */
-    public UTXOPool getMaxHeightUTXOPool() {
-        // IMPLEMENT THIS
-    }
+  /** Get the transaction pool to mine a new block */
+  public TransactionPool getTransactionPool() {
+    return transactionPool;
+  }
 
-    /** Get the transaction pool to mine a new block */
-    public TransactionPool getTransactionPool() {
-        // IMPLEMENT THIS
-    }
+  /**
+   * Add {@code block} to the block chain if it is valid. For validity, all transactions should be
+   * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}.
+   *
+   * <p>For example, you can try creating a new block over the genesis block (block height 2) if the
+   * block chain height is {@code <= CUT_OFF_AGE + 1}. As soon as {@code height > CUT_OFF_AGE + 1},
+   * you cannot create a new block at height 2.
+   *
+   * @return true if block is successfully added
+   */
+  public boolean addBlock(Block block) {
+    try {
+      if (block.getPrevBlockHash() == null) {
+        return false;
+      }
+      ByteArrayWrapper parentHash = new ByteArrayWrapper(block.getPrevBlockHash());
+      Integer heightOfParent = blockHeightMap.get(parentHash);
+      if (heightOfParent == null || heightOfParent < minHeightInMem) {
+        return false;
+      }
 
-    /**
-     * Add {@code block} to the block chain if it is valid. For validity, all transactions should be
-     * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}.
-     * 
-     * <p>
-     * For example, you can try creating a new block over the genesis block (block height 2) if the
-     * block chain height is {@code <=
-     * CUT_OFF_AGE + 1}. As soon as {@code height > CUT_OFF_AGE + 1}, you cannot create a new block
-     * at height 2.
-     * 
-     * @return true if block is successfully added
-     */
-    public boolean addBlock(Block block) {
-        // IMPLEMENT THIS
+      UTXOPool copyOfParentUTXOPool = new UTXOPool(utxoPoolMap.get(parentHash));
+      copyOfParentUTXOPool.addUTXO(
+          new UTXO(block.getCoinbase().getHash(), 0), block.getCoinbase().getOutput(0));
+      TxHandler txHandler = new TxHandler(copyOfParentUTXOPool);
+      List<Transaction> validTransactions =
+          Arrays.asList(txHandler.handleTxs(block.getTransactions().toArray(new Transaction[0])));
+      if (validTransactions.size() == block.getTransactions().size()) {
+        utxoPoolMap.put(new ByteArrayWrapper(block.getHash()), txHandler.getUTXOPool());
+        blockHeightMap.put(new ByteArrayWrapper(block.getHash()), heightOfParent + 1);
+      } else {
+        return false;
+      }
+      if (heightOfParent == minHeightInMem + CUT_OFF_AGE - 1) {
+        minHeightInMem++;
+        blocksInMemory.take();
+      }
+      if (heightOfParent == minHeightInMem + blocksInMemory.size() - 1) {
+        List<Block> latestBlocks = new ArrayList<>();
+        latestBlocks.add(block);
+        blocksInMemory.offer(latestBlocks);
+        return true;
+      }
+      Iterator<List<Block>> it = blocksInMemory.iterator();
+      List<Block> currentItem = it.next();
+      for (int i = 0; i <= heightOfParent - minHeightInMem; i++) {
+        currentItem = it.next();
+      }
+      currentItem.add(block);
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return false;
+  }
 
-    /** Add a transaction to the transaction pool */
-    public void addTransaction(Transaction tx) {
-        // IMPLEMENT THIS
-    }
+  /** Add a transaction to the transaction pool */
+  public void addTransaction(Transaction tx) {
+    transactionPool.addTransaction(tx);
+  }
 }
---
+++ assignment3/TxHandler.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TxHandler {
+
+  private UTXOPool utxoPool;
+  private Function<Transaction, List<UTXO>> getUTXOsClaimedByTx =
+      transaction ->
+          transaction
+              .getInputs()
+              .stream()
+              .map(in -> new UTXO(in.prevTxHash, in.outputIndex))
+              .collect(Collectors.toList());
+
+  /**
+   * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+   * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+   * constructor.
+   */
+  public TxHandler(UTXOPool utxoPool) {
+    this.utxoPool = new UTXOPool(utxoPool);
+  }
+  
+  public UTXOPool getUTXOPool() {
+    return utxoPool;
+  }
+  /**
+   * @return true if: (1) all outputs claimed by {@code tx} are in the current UTXO pool, (2) the
+   *     signatures on each input of {@code tx} are valid, (3) no UTXO is claimed multiple times by
+   *     {@code tx}, (4) all of {@code tx}s output values are non-negative, and (5) the sum of
+   *     {@code tx}s input values is greater than or equal to the sum of its output values; and
+   *     false otherwise.
+   */
+  public boolean isValidTx(Transaction tx) {
+
+    // 1
+    Predicate<Transaction> utxosClaimedByTxAreInPool =
+        transaction -> utxoPool.getAllUTXO().containsAll(getUTXOsClaimedByTx.apply(transaction));
+
+    // 2
+    Predicate<Transaction> allInputsHaveValidSignature =
+        transaction ->
+            transaction
+                .getInputs()
+                .stream()
+                .allMatch(
+                    in ->
+                        Crypto.verifySignature(
+                            utxoPool.getTxOutput(new UTXO(in.prevTxHash, in.outputIndex)).address,
+                            transaction.getRawDataToSign(transaction.getInputs().indexOf(in)),
+                            in.signature));
+
+    // 3
+    Predicate<Transaction> noUTXOisClaimedMultipleTimes =
+        transaction ->
+            getUTXOsClaimedByTx.apply(transaction).size()
+                == getUTXOsClaimedByTx.andThen(HashSet::new).apply(transaction).size();
+
+    // 4
+    Predicate<Transaction> allOutputValuesAreNonNegative =
+        transaction ->
+            transaction.getOutputs().stream().map(o -> o.value).noneMatch(value -> value < 0.0);
+
+    // 5
+    Predicate<Transaction> totalOutputValueLessThanTotalInputValue =
+        transaction ->
+            transaction.getOutputs().stream().mapToDouble(o -> o.value).sum()
+                < getUTXOsClaimedByTx
+                    .apply(transaction)
+                    .stream()
+                    .map(utxoPool::getTxOutput)
+                    .mapToDouble(o -> o.value)
+                    .sum();
+
+    return utxosClaimedByTxAreInPool
+        .and(allInputsHaveValidSignature)
+        .and(noUTXOisClaimedMultipleTimes)
+        .and(allOutputValuesAreNonNegative)
+        .and(totalOutputValueLessThanTotalInputValue)
+        .test(tx);
+  }
+
+  /**
+   * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+   * transaction for correctness, returning a mutually valid array of accepted transactions, and
+   * updating the current UTXO pool as appropriate.
+   */
+  public Transaction[] handleTxs(Transaction[] possibleTxs) {
+    List<Transaction> validTransactions = new ArrayList<>();
+    List<Transaction> possibleTransactions = new ArrayList<>(Arrays.asList(possibleTxs));
+    List<Transaction> validTxInThisIter;
+    while (!(validTxInThisIter = handleTxsIter(possibleTransactions)).isEmpty()) {
+      validTransactions.addAll(validTxInThisIter);
+      possibleTransactions.removeAll(validTxInThisIter);
+    }
+    return validTransactions.toArray(new Transaction[validTransactions.size()]);
+  }
+
+  private List<Transaction> handleTxsIter(List<Transaction> possibleTxs) {
+    List<Transaction> validTxsInThisIter = new ArrayList<>();
+    possibleTxs
+        .stream()
+        .filter(this::isValidTx)
+        .forEach(
+            tx -> {
+              tx.getInputs()
+                  .stream()
+                  .map(in -> new UTXO(in.prevTxHash, in.outputIndex))
+                  .forEach(utxoPool::removeUTXO);
+              IntStream.range(0, tx.getOutputs().size())
+                  .forEach(i -> utxoPool.addUTXO(new UTXO(tx.getHash(), i), tx.getOutput(i)));
+              validTxsInThisIter.add(tx);
+            });
+    return validTxsInThisIter;
+  }
+}
